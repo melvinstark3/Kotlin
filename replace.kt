4c70d6d812216450d27945adda4f64ccd74046a0
Melvin <kartik3163@gmail.com>
2019-08-30 00:21:08 +0530

    kotlin: Strings and .replace

---
+++ replace.kt
@@ -0,0 +1,5 @@
+fun main(){
+    var s1 = "Melvin is here" //Defined a string
+    var s2 = "${s1.replace("is","was")}, but now he is gone" //A string using .replace
+    print (s2) //print the new modified string
+}
