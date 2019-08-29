8acce62c36b35f2f52f24cf06aa02a4e210cddf0
Melvin <kartik3163@gmail.com>
2019-08-29 23:52:14 +0530

    kotlin: Local Variables
    Read-only local variables are defined using the keyword 'val'.

---
+++ abc.kt
@@ -0,0 +1,7 @@
+fun main(){
+    val a: Int=1 //defined a variable 'a' with immediate assignment
+    val b = 2 //Int type is inferred when value is directly assigned
+    val c: Int //defined a variable
+    c=3 //Assigned the value
+    print("a=$a, b=$b, c=$c") //print
+}
