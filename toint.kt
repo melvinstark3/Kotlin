9080bbf941401596925f6d0634cbffa2db1c73ef
Melvin <kartik3163@gmail.com>
2019-09-01 00:35:53 +0530

    kotlin: Usage of .toInt
    .toInt Parses the string as an Int number and returns the result.

---
+++ toint.kt
@@ -0,0 +1,9 @@
+fun conv(ryuk: String): Int{
+    //use .toInt to convert string into Int
+    return ryuk.toInt()
+}
+fun main(){
+    //String containing Numbers only
+    var eww: String = "1230"
+    print(conv(eww))
+}
