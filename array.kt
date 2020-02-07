d03eb2fdfe678a614fc8572b1acf1e0bc99fe99e
Melvin <kartik3163@gmail.com>
2020-02-07 13:33:23 +0530

    Use while loop to print array

---
+++ array.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>){
+    var students= arrayOf("Mike","Cody","Jimmy")
+    print(students.size)
+    var counter = 0
+        while(counter < students.size){
+        println(students[counter])
+        counter++
+    }
+}
--- size.kt
+++
@@ -1,4 +0,0 @@
-fun main(args: Array<String>){
-    var students= arrayOf("Mike","Cody","Jimmy")
-    print(students.size)
-}
