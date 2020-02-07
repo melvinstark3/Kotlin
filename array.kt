8c590cddf4ab3d2e566b3d42a86c2d9f21d261ce
Melvin <kartik3163@gmail.com>
2020-02-07 13:40:08 +0530

    do-while loop

--- array.kt
+++ array.kt
@@ -1,9 +1,13 @@
-fun main(args: Array<String>){
+fun main(args: Array<String>)
+{
     var students= arrayOf("Mike","Cody","Jimmy")
-    print(students.size)
     var counter = 0
-        while(counter < students.size){
+    println("Total number of students are "+students.size)
+    println("Name of students are : ")
+    //1st loop with be executed without checking any condition
+    do {
         println(students[counter])
         counter++
     }
+    while(counter < students.size)
 }
