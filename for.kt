3ad30060b5aca9b06f4def73d884055841a018b8
Melvin <kartik3163@gmail.com>
2020-02-07 14:05:10 +0530

    Iterate list using for loop

--- for.kt
+++ for.kt
@@ -1,8 +1,8 @@
 fun main(args: Array<String>) {
 
-    var planets = arrayOf("Earth", "Mars", "Venus", "Jupiter", "Saturn")
-    // indices keyword is used to get the range/index values of any array.
-    for (i in planets.indices) {
-        println("Element")
+    // list is a collection of read only variables with fixed size
+    var list = listOf(1,2,"Melvin")
+    for(i in list){
+        println(i)
+    }
 }
-    
