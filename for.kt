4464145a961ac08c084575df2a7cee76af84c333
Melvin <kartik3163@gmail.com>
2020-02-07 13:49:22 +0530

    Use of step keyword

--- for.kt
+++ for.kt
@@ -1,7 +1,8 @@
 fun main(args: Array<String>) {
     
     // use .. to create a range
-    for (i in 1..10) {
+    // use step keyword to skip elements in any range
+    for (i in 1..10 step 3) {
         print("$i ")
     }
 }
