e89c3a0061d315ec22b6666f808b36cfa59be9ae
Melvin <kartik3163@gmail.com>
2020-02-07 13:45:35 +0530

    Use for loop to print an array

---
+++ for.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    var count = intArrayOf(1,2,3,4,5,6,7,8,9)
+    
+    // in operator used in for loop to check value lies within the Range or not.
+    for (i in count) {
+        print("$i ")
+    }
+}
